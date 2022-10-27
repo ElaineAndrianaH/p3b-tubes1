be2acc78b50c5b0651c93849b2366eae17771aca
ilen7 <elaineandriana378@gmail.com>
2022-10-27 21:02:04 +0700

    pindahin save dan load data ke model dokter

--- app/src/main/java/com/example/tubes1/model/dokter.java
+++ app/src/main/java/com/example/tubes1/model/dokter.java
@@ -1,16 +1,26 @@
 package com.example.tubes1.model;
 
+import android.content.Context;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 
 public class dokter {
     private String nama;
     private String sp;
     private String tlp;
+    private static String filename = "dokter.data";
 
     public dokter(String nama, String sp, String tlp) {
         this.nama = nama;
@@ -40,5 +50,64 @@ public class dokter {
 
     public void setTlp(String tlp) {
         this.tlp = tlp;
+    }
+
+    @Override
+    public String toString() {
+        return this.nama + "," + this.sp + "," + this.tlp + ";";
+    }
+    public static void saveData(Context context, List<dokter>list) {
+        File file;
+        FileOutputStream fot= null;
+        try {
+            file= new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
+            fot= new FileOutputStream(file);
+            if(!file.exists()){
+                file.createNewFile();
+            }
+            String textToWrite="";
+            for (int i = 0; i < list.size(); i++) {
+                textToWrite+=list.get(i).toString();
+            }
+            fot.write(textToWrite.getBytes());
+            fot.flush();
+            fot.close();
+            Log.d("masuk",file+"");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    public static List<dokter> loadData(Context context){
+        List<dokter>list=new LinkedList<dokter>();
+        //load external storage
+        File file= new File(context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
+        try{
+            if(!file.exists()){
+                file.createNewFile();
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(fis, StandardCharsets.UTF_8);
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            if(line!=null){
+                String[] res=line.split(";");
+                for (int i = 0; i < res.length; i++) {
+                    String[] resData=res[i].split(",");
+                    list.add(new dokter(res[0],res[1],res[2]));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
     }
 }
--- app/src/main/java/com/example/tubes1/presenter/DokterPresenter.java
+++ app/src/main/java/com/example/tubes1/presenter/DokterPresenter.java
@@ -23,14 +23,12 @@ import java.util.List;
 public class DokterPresenter {
     private DokterUI ui;
     private List<dokter> list;
-    private static String filename="dokter.data";
     private Context context;
 
     public DokterPresenter(DokterUI ui, Context context) {
         this.ui = ui;
         this.list= new LinkedList<dokter>();
         this.context = context;
-        saveData();
         loadData();
     }
 
@@ -38,55 +36,11 @@ public class DokterPresenter {
         this.ui.listenerOnClick(page);
     }
 
-    public void loadData(){
-        //load external storage
-        File file= new File(this.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        InputStreamReader inputStreamReader =
-                new InputStreamReader(fis, StandardCharsets.UTF_8);
-        StringBuilder stringBuilder = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
-            String line = reader.readLine();
-            while (line != null) {
-                stringBuilder.append(line).append('\n');
-                line = reader.readLine();
-
-                Log.d("masuk","");
-            }
-        } catch (IOException e) {
-            // Error occurred when opening raw file for reading.
-        } finally {
-            String contents = stringBuilder.toString();
-        }
-        this.ui.updateList(list);
-
-    }
-
     public void saveData(){
-        File file;
-        FileOutputStream fot= null;
-        try {
-            file= new File(this.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
-            fot= new FileOutputStream(file);
-            if(!file.exists()){
-                file.createNewFile();
-            }
-            String textToWrite="dr.a,a,123";
-//            this.list.forEach((dokter)->{
-//                Log.d("masuk", "saveData: ");
-//            });
-            fot.write(textToWrite.getBytes());
-            fot.flush();
-            fot.close();
-            Log.d("masuk",file+"");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+        dokter.saveData(context,this.list);
+    }
+    public void loadData() {
+        this.ui.updateList(dokter.loadData(context));
     }
 
 }
