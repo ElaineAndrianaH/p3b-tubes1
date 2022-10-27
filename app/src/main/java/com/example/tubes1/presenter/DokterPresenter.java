c99ca24a88fde7c9e764e1c0a36dae314bc0a967
Joxazzz14 <95340630+Joxazzz14@users.noreply.github.com>
2022-10-27 20:04:28 +0700

    Load Data dan Save Data Android Studio

--- app/src/main/java/com/example/tubes1/presenter/DokterPresenter.java
+++ app/src/main/java/com/example/tubes1/presenter/DokterPresenter.java
@@ -11,6 +11,7 @@ import com.example.tubes1.model.dokter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -24,32 +25,52 @@ public class DokterPresenter {
     private List<dokter> list;
     private static String filename="dokter.data";
     private Context context;
+
     public DokterPresenter(DokterUI ui, Context context) {
         this.ui = ui;
         this.list= new LinkedList<dokter>();
         this.context = context;
         saveData();
+        loadData();
     }
+
     public void changeListener(String page){
         this.ui.listenerOnClick(page);
     }
+
     public void loadData(){
         //load external storage
         File file= new File(this.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
-        try (FileInputStream fis = new FileInputStream(file));
-
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader =
+                new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+                Log.d("masuk","");
+            }
         } catch (IOException e) {
             // Error occurred when opening raw file for reading.
         } finally {
             String contents = stringBuilder.toString();
         }
         this.ui.updateList(list);
+
     }
+
     public void saveData(){
         File file;
         FileOutputStream fot= null;
         try {
-            file= new File(this.context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),filename);
+            file= new File(filename);
             fot= new FileOutputStream(file);
             if(!file.exists()){
                 file.createNewFile();
